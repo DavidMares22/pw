237c58a35c9787480a4b4adce6a643f0938601e6
David Mares <damr18@gmail.com>
2025-05-24 09:53:40 -0600

    example of events input and change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pw';
+  includeLetters = false;
+  password = '';
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  passLength: number = 0;
+
+ 
+  onButtonClick() {
+    console.log(`
+      Include Letters: ${this.includeLetters}
+      Include Numbers: ${this.includeNumbers}
+      Include Symbols: ${this.includeSymbols}
+      Password Length: ${this.passLength}
+    `);
+    
+  }
+
+  onChangeUseLetters(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.includeLetters = inputElement.checked;
+}
+  onChangeUseNumbers(event: Event) {
+    // console.log('Checkbox changed!', event.target.checked);
+    const inputElement = event.target as HTMLInputElement;
+    this.includeNumbers = inputElement.checked;
+  }
+  onChangeUseSymbols(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.includeSymbols = inputElement.checked;
+  }
+  onChangeLength(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const parsedValue = parseInt(inputElement.value);
+    if (!isNaN(parsedValue)) {
+      this.passLength = parsedValue;
+    }
+  }
 }
